90194fd0703f3e59374a900d1e7fe25f7dda1cdd
rohitryali <rohitryali9@gmail.com>
2025-12-10 12:57:25 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,6 @@ yum install java-17-amazon-corretto -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install jenkins
+yum install jenkins -y
 systemctl start jenkins
 systemctl status jenkins
