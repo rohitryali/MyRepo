ba24c52f1ec0ad8eead601bf9183da4edc9c970e
rohitryali <rohitryali9@gmail.com>
2026-02-22 00:48:05 +0530

    Update Nexus installation to version 3.89.1-02

--- nexus.sh
+++ nexus.sh
@@ -1,9 +1,10 @@
 yum install java-17-amazon-corretto -y
 cd /opt
-wget https://download.sonatype.com/nexus/3/nexus-3.87.1-01-linux-x86_64.tar.gz
-tar -zxvf nexus-3.87.1-01-linux-x86_64.tar.gz
+wget https://download.sonatype.com/nexus/3/nexus-3.89.1-02-linux-x86_64.tar.gz
+tar -zxvf nexus-3.89.1-02-linux-x86_64.tar.gz
 useradd rohit
-chown -R rohit:rohit nexus-3.87.1-01 sonatype-work
+chown -R rohit:rohit nexus-3.89.1-02 sonatype-work
 su - rohit
-cd /opt/nexus-3.87.1-01/bin
+cd /opt/nexus-3.89.1-02/bin
 ./nexus start
+
